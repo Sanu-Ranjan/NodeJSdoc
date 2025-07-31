69e8a01cc410ab539e777383518271f80ff57e5c
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-08-01 00:34:36 +0530

    Express- logging request line in a file by creating writable stream

--- Express/myapp/app.js
+++ Express/myapp/app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 
+const fs = require("fs");
+const path = require("path");
+
+//create a writable stream
+const logStream = fs.createWriteStream(path.join(__dirname, "server.log"), {
+  flags: "a",
+});
+
 const app = express();
 
 const port = 3000;
 
 app.use((req, res, next) => {
-  console.log(
-    `request line ${req.method} ${req.originalUrl} ${req.httpVersion}`
-  );
+  const timeStamp = new Date();
+  const logLine = ` ${timeStamp.toLocaleString("en-IN")} request line: ${
+    req.method
+  } ${req.originalUrl} ${req.httpVersion}\n`;
+
+  console.log(logLine);
+  logStream.write(logLine);
   next();
 });
 
